98d86ecdd8ca25f6f66a54bc2c8a07c507d155d3
Claudio Campos Furbino Rabelo <claudio.rabelo@rede.local>
2018-07-11 13:09:16 -0300

    trazendo campo

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import data from '../general/data.json';
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
+    <img src={ data.logo } />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
